e03a72efe69479a59b4dfd734dec181b16c9aa50
Josh Darling <joshua.a.darling@gmail.com>
2022-12-31 15:32:40 -0700

    seperate-positive tests passed

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(arr) {
+    let left = 0
+    let right = arr.length - 1
+    while(left < right)
+    if(arr[left] > 0 && arr[right] > 0){
+        left++
+    }
+    else if(arr[left] < 0 && arr[right] > 0){
+        [arr[left],arr[right]] = [arr[right],arr[left]]
+    }
+    else{
+        right--
+    }
+    return arr
+
 }
