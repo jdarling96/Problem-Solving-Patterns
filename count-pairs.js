2f2bf5c38a99c88481e14267c38beb7578503eed
Josh Darling <joshua.a.darling@gmail.com>
2022-12-31 17:03:30 -0700

    count pairs tests passed

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,15 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(arr, target) {
+    let obj = {}
+    let count = 0
+    for (let n of arr){
+        if(obj[n] !== undefined){
+            count++
+        }else{
+            let num = target - n
+            obj[num] = num
+
+        }
+    }
+    return count
 }
--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,35 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(str1, str2) {
+    if(str2.length < str1.length) return false
+
+    function freqCount(str) {
+        let freqCounter = {}
+        let count = 0
+        for(let char of str){
+            if(!freqCounter[char]){
+            freqCounter[char] = count
+            count++
+
+            } 
+            
+        }
+        return freqCounter
+    }
+    let str1Count = freqCount(str1)
+    console.log(str1Count)
+    let str1Visited = 0
+    for(let i = 0; i < str2.length; i++){
+        if(str1Visited === str1.length) return true
+        if(str1Count[str2[i]] !== undefined){
+            if(str1Count[str2[i]] <= i){
+                str1Visited++
+            } else{
+                return false
+            }
+
+        }
+        
+
+    }
+    return true
 }
