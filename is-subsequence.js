729834d977d14f4d10e18450497ea9ad19964410
Josh Darling <joshua.a.darling@gmail.com>
2023-01-01 15:59:26 -0700

    All questions tests passed

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,35 +1,14 @@
 // add whatever parameters you deem necessary
 function isSubsequence(str1, str2) {
-    if(str2.length < str1.length) return false
-
-    function freqCount(str) {
-        let freqCounter = {}
-        let count = 0
-        for(let char of str){
-            if(!freqCounter[char]){
-            freqCounter[char] = count
-            count++
-
-            } 
-            
-        }
-        return freqCounter
+    let str1Idx = 0;
+    let str2Idx = 0;
+    if (!str1) return true;
+    while (str2Idx < str2.length) {
+      if (str2[str2Idx] === str1[str1Idx]) {
+        str1Idx += 1;
+      }
+      if (str1Idx === str1.length) return true;
+      str2Idx += 1;
     }
-    let str1Count = freqCount(str1)
-    console.log(str1Count)
-    let str1Visited = 0
-    for(let i = 0; i < str2.length; i++){
-        if(str1Visited === str1.length) return true
-        if(str1Count[str2[i]] !== undefined){
-            if(str1Count[str2[i]] <= i){
-                str1Visited++
-            } else{
-                return false
-            }
-
-        }
-        
-
-    }
-    return true
-}
+    return false;
+  }
